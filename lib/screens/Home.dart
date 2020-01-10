3d3c0b7b200dfa6de6c4756bc7e0607dae7a2631
CalvinSey <40975050+ChepChanSeyha@users.noreply.github.com>
2020-01-10 15:03:49 +0700

    Update HomeScreen

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -11,7 +11,7 @@ final List<String> imgList = [
 
 final List child = map<Widget>(
   imgList,
-      (index, i) {
+  (index, i) {
     return Container(
       margin: EdgeInsets.all(5.0),
       child: ClipRRect(
@@ -19,18 +19,18 @@ final List child = map<Widget>(
         child: Stack(children: <Widget>[
           Image.asset(i, fit: BoxFit.cover, width: 1000.0),
           Positioned(
-            bottom: 0.0,
-            left: 0.0,
-            right: 0.0,
             child: Container(
               decoration: BoxDecoration(
                 gradient: LinearGradient(
-                  colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                  colors: [
+                    Color.fromARGB(200, 0, 0, 0),
+                    Color.fromARGB(0, 0, 0, 0)
+                  ],
                   begin: Alignment.bottomCenter,
                   end: Alignment.topCenter,
                 ),
               ),
-              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
               child: Text(
                 'No. $index image',
                 style: TextStyle(
@@ -59,7 +59,6 @@ List<T> map<T>(List list, Function handler) {
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
-
 }
 
 class _HomeState extends State<Home> {
@@ -70,73 +69,195 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: SingleChildScrollView(
           child: Container(
-            child: Column(
-              children: <Widget>[
-                Container(
-                    height: MediaQuery.of(context).size.height * 0.2,
-                    width: MediaQuery.of(context).size.width * 1,
-                    decoration: BoxDecoration(
-                      color: Color(0xff26304D),
-                    ),
-                    padding: EdgeInsets.only(top: 5),
-                    child: SafeArea(
-                      child: Column(
-                        children: <Widget>[
-                          Image.asset("assets/images/gic_logo.png",
-                              height:70,width:80
-                          ),
-                          SizedBox(height: 4,),
-                          Text("Génie Informatique et Communication",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontFamily: 'SegoePrint',
-                              fontSize: 16,
-                            ),
-                          )
-                        ],
+        child: Column(
+          children: <Widget>[
+            Container(
+                height: MediaQuery.of(context).size.height * 0.2,
+                width: MediaQuery.of(context).size.width * 1,
+                decoration: BoxDecoration(
+                  color: Color(0xff26304D),
+                ),
+                padding: EdgeInsets.only(top: 5),
+                child: SafeArea(
+                  child: Column(
+                    children: <Widget>[
+                      Image.asset("assets/images/gic_logo.png",
+                          height: 70, width: 80),
+                      SizedBox(
+                        height: 4,
                       ),
-                    )
-                ),
-                Column(
-                  children: <Widget>[
-                    CarouselSlider(
-                      items: child,
-                      autoPlay: true,
-                      enlargeCenterPage: true,
-                      aspectRatio: 2.0,
-                      onPageChanged: (index) {
-                        setState(() {
-                          _current = index;
-                        });
-                      },
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: map<Widget>(
-                    imgList,
-                        (index, url) {
-                      return Container(
-                        width: 8.0,
-                        height: 8.0,
-                        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: _current == index
-                                ? Color.fromRGBO(0, 0, 0, 0.9)
-                                : Color.fromRGBO(0, 0, 0, 0.4)),
-                      );
-                    },
+                      Text(
+                        "Génie Informatique et Communication",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontFamily: 'SegoePrint',
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
                   ),
+                )),
+            Column(
+              children: <Widget>[
+                CarouselSlider(
+                  items: child,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  aspectRatio: 2.0,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
                 ),
               ],
             ),
-
-          )
-      ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: map<Widget>(
+                imgList,
+                (index, url) {
+                  return Container(
+                    width: 8.0,
+                    height: 8.0,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: _current == index
+                            ? Color.fromRGBO(0, 0, 0, 0.9)
+                            : Color.fromRGBO(0, 0, 0, 0.4)),
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15, left: 15),
+              child: Container(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Features",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                      color: Color(0xffA61D37)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15, left: 15),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        width: 125,
+                        height: 125,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          color: Colors.white,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                title: Text('News/Events',
+                                    style: TextStyle(color: Color(0xff26304D),)),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 125,
+                        height: 125,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          color: Colors.white,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                title: Text('Heart Shaker',
+                                    style: TextStyle(color: Color(0xff26304D))),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 125,
+                        height: 125,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          color: Colors.white,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                title: Text('Heart Shaker',
+                                    style: TextStyle(color: Color(0xff26304D))),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        width: 125,
+                        height: 125,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          color: Colors.white,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                title: Text('Heart Shaker',
+                                    style: TextStyle(color: Color(0xff26304D),)),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 125,
+                        height: 125,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          color: Colors.white,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                title: Text('Heart Shaker',
+                                    style: TextStyle(color: Color(0xff26304D))),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )),
     );
   }
-
 }
