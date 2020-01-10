0845e285c31af8d6d7f31e2005e0147957782761
shurricanex <sarakkornsakol@gmail.com>
2020-01-10 14:08:23 +0700

    use carousel slider again

--- lib/screens/carousel_slider.dart
+++ lib/screens/carousel_slider.dart
@@ -6,7 +6,6 @@ final List<String> imgList = [
   'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
   'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
   'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
   'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
 ];
 
@@ -15,36 +14,35 @@ final List child = map<Widget>(
   imgList,
   (index, i) {
     return Container(
-      height:3000,
-      width:8000,
-      margin: EdgeInsets.symmetric(vertical:5.0),
+     
+      margin: EdgeInsets.all(5.0),
       child: ClipRRect(
         borderRadius: BorderRadius.all(Radius.circular(20.0)),
         child: Stack(children: <Widget>[
-          Image.network(i,height:1000,width: 7000.0),
-          // Positioned(
-          //   bottom: 0.0,
-          //   left: 0.0,
-          //   right: 0.0,
-          //   child: Container(
-          //     decoration: BoxDecoration(
-          //       gradient: LinearGradient(
-          //         colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
-          //         begin: Alignment.bottomCenter,
-          //         end: Alignment.topCenter,
-          //       ),
-          //     ),
-          //     padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-          //     child: Text(
-          //       'No. $index image',
-          //       style: TextStyle(
-          //         color: Colors.white,
-          //         fontSize: 20.0,
-          //         fontWeight: FontWeight.bold,
-          //       ),
-          //     ),
-          //   ),
-          // ),
+          Image.network(i,fit: BoxFit.cover,width: 1000.0),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              // child: Text(
+              //   'No. $index image',
+              //   style: TextStyle(
+              //     color: Colors.white,
+              //     fontSize: 20.0,
+              //     fontWeight: FontWeight.bold,
+              //   ),
+              // ),
+            ),
+          ),
         ]),
       ),
     );
@@ -75,7 +73,7 @@ class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
         
         items: child,
         autoPlay: true,
-        // enlargeCenterPage: true,
+        enlargeCenterPage: true,
         // aspectRatio: 1.9,
         onPageChanged: (index) {
           setState(() {
